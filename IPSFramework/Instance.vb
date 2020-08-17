fd1734a05647efbfd5e64dce0c2023077c309c75
Tobias Weiss <tobias@tobias-weiss.org>
2020-08-17 17:23:01 +0200

    autocommit

---
+++ IPSFramework/Instance.vb
@@ -0,0 +1,96 @@
+﻿Imports System.Xml
+
+Public Class Instance
+    Public Structure orders 'structure for customer orders
+        Public ID As Short 'self-explanatory
+        Public OrderSKU() As Integer 'SKU per customer order 
+    End Structure
+
+    Public Name As String 'ID of instance
+    Public SKUs As Integer 'number of different SKU
+    Public PickingOrders As Integer 'number of customer orders
+
+    Public Tours As Integer 'max number of tours
+    Public PickingCapacity As Integer 'self-explanatory
+    Public Order() As orders 'customer orders
+    Private xmlpath As String = "" 'loding path
+
+
+    Sub LoadInst(ByVal loadPath As String)
+
+        ReDim Me.Order(0)
+
+        Me.xmlpath = loadPath
+
+        Dim reader As System.IO.StreamReader
+        reader = New System.IO.StreamReader( _
+        Me.xmlpath, _
+        System.Text.Encoding.Unicode, _
+        False)
+
+
+        'xmlObjekte 
+        Dim xmlDoc As XmlDocument
+        Dim xmlRoot As XmlNode
+        Dim xmlNodelist1 As XmlNodeList
+        Dim xmlNodelist2 As XmlNodeList
+        Dim xmlNodelist3 As XmlNodeList
+        Dim indexOrder As Short = 0
+        Dim indexOrderProd As Short = 0
+
+        xmlDoc = New XmlDocument()
+        xmlDoc.Load(Me.xmlpath)
+
+        xmlRoot = xmlDoc.DocumentElement()
+
+        For Each xmlattribute In xmlRoot.Attributes '
+            Me.Name = xmlattribute.value
+        Next
+
+        xmlNodelist1 = xmlRoot.ChildNodes
+
+        For Each node1 As XmlNode In xmlNodelist1
+
+            If node1.Name = "descriptor" Then
+                xmlNodelist2 = node1.ChildNodes
+                For Each node1Undernode As XmlNode In xmlNodelist2
+                    If node1Undernode.Name = "CountProducts" Then
+                        For Each xmlattribute In node1Undernode.Attributes '
+                            Me.SKUs = CInt(xmlattribute.value)
+                        Next
+                    ElseIf node1Undernode.Name = "CountOrders" Then
+                        For Each xmlattribute In node1Undernode.Attributes '
+                            Me.PickingOrders = CInt(xmlattribute.value)
+                        Next
+                    ElseIf node1Undernode.Name = "Capacity" Then
+                        For Each xmlattribute In node1Undernode.Attributes '
+                            Me.PickingCapacity = CInt(xmlattribute.value)
+                        Next
+                    ElseIf node1Undernode.Name = "MaxTours" Then
+                        For Each xmlattribute In node1Undernode.Attributes '
+                            Me.Tours = CInt(xmlattribute.value)
+                        Next
+                    End If
+                Next
+            ElseIf node1.Name = "Orders" Then
+                xmlNodelist2 = node1.ChildNodes
+                For Each node1Undernode As XmlNode In xmlNodelist2
+                    ReDim Preserve Me.Order(indexOrder)
+                    Me.Order(indexOrder).ID = indexOrder
+                    indexOrderProd = 0
+                    xmlNodelist3 = node1Undernode.ChildNodes
+                    For Each nUndernode As XmlNode In xmlNodelist3
+                        ReDim Preserve Me.Order(indexOrder).OrderSKU(indexOrderProd)
+
+                        For Each xmlattribute In nUndernode.Attributes '
+                            Me.Order(indexOrder).OrderSKU(indexOrderProd) = CInt(xmlattribute.value)
+                        Next
+                        indexOrderProd += 1
+                    Next
+                    indexOrder += 1
+                Next
+            End If
+        Next
+
+    End Sub
+End Class
---
+++ IPSFramework/main.vb
@@ -0,0 +1,37 @@
+﻿Module main
+
+    Sub Main()
+
+        For Each foundFile As String In My.Computer.FileSystem.GetFiles(CurDir() & "\Data\") 'batch load
+            Dim NewInstance As New Instance
+
+            NewInstance.LoadInst(foundFile)
+
+            Dim currentSkuOrder = initSolution(NewInstance)
+            Dim currentObj = getObjectiveValue(currentSkuOrder, NewInstance)
+
+        Next
+
+    End Sub
+
+    Sub ameisen()
+        ' Anzahl NBL = Polynomialkoeffizient, d.h. (Anzahl SKUs - Anzahl Plätze + 1) P 2
+
+    End Sub
+
+    Function initSolution(ByRef instance As Instance) As List(Of Integer)
+        Dim skuOrder As New List(Of Integer)
+        For index = 1 To instance.SKUs
+            skuOrder.Add(index)
+        Next
+        Return skuOrder
+    End Function
+
+    Function getObjectiveValue(ByVal skuOrder As List(Of Integer), ByRef NewInstance As Instance) As Integer
+        Dim obj As Integer ' ZFW
+
+        Return obj
+    End Function
+
+
+End Module
